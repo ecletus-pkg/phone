823e0991d34b134174e869edec4fb1d17e8a7516
Moises P. Sena <moisespsena@gmail.com>
2018-08-20 11:58:24 -0300

    Add sources

---
+++ plugin.go
@@ -0,0 +1,23 @@
+package phone
+
+import (
+	"github.com/aghape/admin"
+	"github.com/aghape/admin/adminplugin"
+	"github.com/aghape/db"
+	"github.com/aghape/plug"
+)
+
+type Plugin struct {
+	plug.EventDispatcher
+	db.DBNames
+	adminplugin.AdminNames
+}
+
+func (p *Plugin) OnRegister() {
+	p.AdminNames.OnInitResources(p, func(e *adminplugin.AdminEvent) {
+		e.Admin.AddResource(&QorPhone{}, &admin.Config{Setup: PrepareResource})
+	})
+	db.DisNames(p).DBOnMigrateGorm(func(e *db.GormDBEvent) error {
+		return e.DB.AutoMigrate(&QorPhone{}).Error
+	})
+}
