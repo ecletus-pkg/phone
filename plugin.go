a7b223d38b9a4ab4c4a38699910cd70e08a1043a
Moises P. Sena <moisespsena@gmail.com>
2018-08-24 09:45:29 -0300

    Update admin_plugin

--- plugin.go
+++ plugin.go
@@ -2,7 +2,7 @@ package phone
 
 import (
 	"github.com/aghape/admin"
-	"github.com/aghape/admin/adminplugin"
+	"github.com/aghape-pkg/admin"
 	"github.com/aghape/db"
 	"github.com/aghape/plug"
 )
@@ -10,11 +10,11 @@ import (
 type Plugin struct {
 	plug.EventDispatcher
 	db.DBNames
-	adminplugin.AdminNames
+	admin_plugin.AdminNames
 }
 
 func (p *Plugin) OnRegister() {
-	p.AdminNames.OnInitResources(p, func(e *adminplugin.AdminEvent) {
+	admin_plugin.Events(p).InitResources(func(e *admin_plugin.AdminEvent) {
 		e.Admin.AddResource(&Phone{}, &admin.Config{Setup: PrepareResource, Invisible:true})
 	})
 	db.Events(p).DBOnMigrateGorm(func(e *db.GormDBEvent) error {
